e5925520c89266f9ae865b2be6c9452e3ad85202
♡ Ana Beltrán ♡ <114430768+AnaBeltran24@users.noreply.github.com>
2022-10-03 12:47:09 -0500

    Ejercicios
    Ejercicios

---
+++ Taller de estructuras de control secuenciales/area.py
@@ -0,0 +1,9 @@
+import math
+#Entradas
+ladou=float(input("Ingrese lado 1: "))
+ladod=float(input("Ingrese lado 2: "))
+ladot=float(input("Ingrese lado 3: "))
+#Caja negra
+s= (ladou+ladod+ladot)/2
+area= math.sqrt(s*(s-ladou)*(s-ladod)*(s-ladot))
+print("El area es: "+str(area))
